29668de3bf16e44d941953c5cdd9068ab3ec9824
linoandy <linoandy@Ruis-MacBook-Pro.local>
2016-09-08 19:03:33 -0400

    add a corpus of sentence with boundary

--- clean_data.py
+++ clean_data.py
@@ -2,8 +2,9 @@ import re
 import glob
 from nltk import tokenize
 
-corpus_sentence = []
 corpus_word = []
+corpus_sentence = []
+corpus_sentence_boundary = []
 
 corpusName = raw_input('Please enter the type of corpus: ')
 path = "./data_corrected/classification_task/%s/train_docs/*.txt" % (corpusName)
@@ -13,7 +14,7 @@ for filename in glob.glob(path):
 			# find "From : email" and replace them with empty string
 			email = re.findall(r"From\s*:\s*[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+\s", line)
 			if len(email) > 0:
-				line = line.replace(email[0].encode('utf-8'), '')
+				line = line.replace(email[0].encode('utf-8'), '', 1)
 
 			# find first "Subject : " and replace with empty string
 			line = line.replace('Subject : ', '', 1)
@@ -29,5 +30,13 @@ for filename in glob.glob(path):
 
 print corpus_word # this is a list of words
 print corpus_sentence # this is a list of sentences
+
+# create a corpus_sentence with <s> and </s>
+for sentence in corpus_sentence:
+	sentence = '<s> ' + sentence.encode('utf-8') + ' </s>'
+	corpus_sentence_boundary.append(sentence)
+
+print corpus_sentence_boundary # this is a list of sentences with <s> and </s>
+
 
 
