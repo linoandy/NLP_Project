0fc09f64d16c1747186a0f81625f50f062cfba92
giyoonhan <giyoonhan@gmail.com>
2016-09-11 21:39:11 -0400

    unigrams, bigrams choosing from probabilities, instead of raw count

--- clean_data.py
+++ clean_data.py
@@ -55,14 +55,18 @@ for sentence in corpus_sentence:
 unigram_counter = collections.Counter(corpus_word)
 unigram_counter_sum = sum(unigram_counter.values())
 
+# for each unigram, compute the probability of each
+for key in unigram_counter:
+	unigram_counter[key] /= float(unigram_counter_sum)
+
 # randomly select a unigram from corpus
 def rand_select_unigram ():
-	# random integer [0..(counter_sum - 1)]
-	rand_int = random.randrange(0, unigram_counter_sum)
+	# [0..1)
+	rand_float = random.random()
 	temp = 0
 	for key in unigram_counter:
 		temp += unigram_counter[key]
-		if rand_int < temp:
+		if rand_float < temp:
 			return key
 
 # generate one sentence from unigrams
@@ -70,7 +74,7 @@ def unigram_sentence_generator ():
 	sentence = ''
 	while True:
 		rand_word = rand_select_unigram ()
-		sentence += rand_word + " "
+		sentence += rand_word + ' '
 		if rand_word == "." or rand_word == "!" or rand_word == "?":
 			return sentence
 
@@ -79,36 +83,48 @@ writetoCSV(['---------------------UNIGRAM---------------------'], corpusName)
 for i in range(5):
 	writetoCSV([unigram_sentence_generator()], corpusName)
 
-# if probabilities are needed:
-#for key in counter:
-#	counter[key] /= float(counter_sum)
+#for i in range(10):
+#	print unigram_sentence_generator()
 
 ### BIGRAM RANDOM SENTENCE GENERATION ###
 
 bigram_list = []
+# unigram list with <s> and </s>
+unigram_list_with_boundary = []
 
 # build bigram list from boundary-applied sentences
 for sentence in corpus_sentence_boundary:
 	word_list = sentence.split(' ')
 	bigram_in_sentence = []
+	unigram_in_sentence = []
 	for i in range(1, len(word_list)):
 		# append tuple (prev, next)
 		bigram_in_sentence.append((word_list[i-1], word_list[i]))
-	# add bigrams from each sentence to the bigram list
+	for i in range(0, len(word_list)):
+		unigram_in_sentence.append(word_list[i])
+	# add bigrams, unigrams from each sentence to the cumulative lists
 	bigram_list += bigram_in_sentence
+	unigram_list_with_boundary += unigram_in_sentence
+
+bigram_counter = collections.Counter(bigram_list)
+unigram_with_boundary_counter = collections.Counter(unigram_list_with_boundary)
+
+# a dictionary of bigram probabilities
+# e.g. {('I', 'am') : 0.3} means that P(am | I) = 0.3.
+bigram_probabilities = dict()
+for key in bigram_counter:
+	bigram_probabilities[key] = bigram_counter[key] / float(unigram_with_boundary_counter[key[0]])
 
 # randomly select a bigram, given the previous word
 def rand_select_bigram (given_word):
-	# selecting only the bigrams that start with the given word
-	selected_bigram_list = [x for x in bigram_list if x[0] == given_word]
-	counter = collections.Counter(selected_bigram_list)
-	counter_sum = sum(counter.values())
-	# random integer [0..(counter_sum - 1)]
-	rand_int = random.randrange(0, counter_sum)
+	# selecting only the bigram probabilities that start with the given word
+	selected_bigram_probabilities = \
+	dict((k,v) for k,v in bigram_probabilities.items() if k[0] == given_word)
+	rand_float = random.random()
 	temp = 0
-	for key in counter:
-		temp += counter[key]
-		if rand_int < temp:
+	for key in selected_bigram_probabilities:
+		temp += selected_bigram_probabilities[key]
+		if rand_float < temp:
 			# return only the newly selected word of the tuple
 			return key[1]
 
@@ -127,4 +143,5 @@ print '---------------------BIGRAM----------------------'
 writetoCSV(['---------------------BIGRAM----------------------'], corpusName)
 for i in range(5):
 	writetoCSV([bigram_sentence_generator()], corpusName)
-
+#for i in range(10):
+#	print bigram_sentence_generator()
