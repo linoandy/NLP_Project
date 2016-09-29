0bba4392c386aa4d72b779aa1e787ef7e35361d9
linoandy <linoandy@dhcp-rhodes-247.eduroam.cornell.edu>
2016-09-29 16:07:36 -0400

    add in precision and recall calculation for baseline

--- NLP_Project2/training.py
+++ NLP_Project2/training.py
@@ -27,19 +27,6 @@ def BIO_tagger(file): # this function processes the document passed in, and repl
 					token_lists[i][2] = 'I-CUE'
 	return token_lists
 
-# baseline system
-baseline_dictionary = []
-path = "./nlp_project2_uncertainty/train/*.txt"
-for file_name in glob.glob(path):
-	training_set_threshold = len(glob.glob(path)) * 0.01
-	if int(re.findall('[0-9]+', file_name)[1]) < training_set_threshold:
-		baseline_set = BIO_tagger(file_name)
-	for baseline in baseline_set:
-		if (baseline[2] == 'B-CUE' or baseline[2] == 'I-CUE') and baseline[0] not in baseline_dictionary:
-			baseline_dictionary.append(baseline[0])
-print baseline_dictionary
-
-
 # breaking up the data set
 training_set = []
 development_set = []
@@ -51,19 +38,57 @@ for file_name in glob.glob(path):
 	else:
 		development_set.append(file_name)
 
-# process training set by BIO tagging and concatenate tokens into sentence
-for filename in training_set:
-	sentence = []
-	list_of_tokens = BIO_tagger(filename)
-	for token in list_of_tokens:
-		sentence.append(token[0])
+# # process training set by BIO tagging and concatenate tokens into sentence
+# for filename in training_set:
+# 	sentence = []
+# 	list_of_tokens = BIO_tagger(filename)
+# 	for token in list_of_tokens:
+# 		sentence.append(token[0])
 
-	# play with it in nltk tagger
-	nltk_pos_tag_result = nltk.pos_tag(sentence)
-	error = 0
-	for i in range(len(list_of_tokens)):
-		if list_of_tokens[i][1] != nltk_pos_tag_result[i][1]:
-			error += 1
-	print filename, ' error: ', error
+# 	# play with it in nltk tagger
+# 	nltk_pos_tag_result = nltk.pos_tag(sentence)
+# 	error = 0
+# 	for i in range(len(list_of_tokens)):
+# 		if list_of_tokens[i][1] != nltk_pos_tag_result[i][1]:
+# 			error += 1
+# 	print filename, ' error: ', error
+
+
+# baseline system
+def baseline_dict(path):
+	baseline_list = []
+	for file_name in glob.glob(path):
+		training_set_threshold = len(glob.glob(path)) * 0.01
+		if int(re.findall('[0-9]+', file_name)[1]) < training_set_threshold:
+			baseline_set = BIO_tagger(file_name)
+		for baseline in baseline_set:
+			if (baseline[2] == 'B-CUE' or baseline[2] == 'I-CUE') and baseline[0] not in baseline_list:
+				baseline_list.append(baseline[0])
+	return baseline_list
+
+def baseline_calculation(file, baseline_data_set):
+	token_lists = []
+	baseline_result = {}
+	with open(file, 'r') as f:
+		for line in f:
+			# split by tab
+			if len(line.split('	')) == 3: # filter out the very last line of each document, usually it's '\n'
+				token_lists.append(line.split('	'))
+
+		correct = 0
+		num_of_prediction = float(len(token_lists))
+		for token_list in token_lists:
+			if token_list[0] in baseline_data_set and token_list[2].find('CUE') != -1:
+				correct += 1
+		baseline_result['file'] = file
+		baseline_result['precision'] = correct / num_of_prediction
+		baseline_result['recall'] = correct / num_of_prediction
+	return baseline_result
+
+baseline_dictionary = baseline_dict("./nlp_project2_uncertainty/train/*.txt")
+for filename in development_set:
+	print baseline_calculation(filename, baseline_dictionary)
+
+
 
 
