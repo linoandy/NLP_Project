9817276bb0da4b29892e3f266b16450aa2481bd1
Gi Yoon Han <giyoonhan@gmail.com>
2016-10-31 19:10:59 -0400

    TODO 1 in progress

--- NLP_Project3/baseline.py
+++ NLP_Project3/baseline.py
@@ -29,8 +29,6 @@ import glob
 			Pattern learning????
 '''
 
-
-
 starting_num = 89
 ending_num = 320
 current_num = starting_num
@@ -39,6 +37,30 @@ current_num = starting_num
 q_path = "./question.txt"
 a_path = "./answer.txt"
 d_path = "./doc_dev"
+
+###############################################################################
+###############################################################################
+# QUESTION PROCESSING
+
+# IN  : Question Description
+# OUT : List of keywords,
+#       just leaving out the question word and stop word list(METHOD 1)
+
+# using a small stop word list; can use a bigger one later if wanted
+stop_word = ['a'
+	]
+
+def get_q_keywords(desc):
+	return
+
+
+
+
+
+
+###############################################################################
+###############################################################################
+# IO
 
 # Types : WHO / WHERE / WHEN
 # OR : PERSON/ LOCATION/ TIME
@@ -49,85 +71,44 @@ def get_questions():
 		num = 0
 		desc_bool = False
 		desc = ''
-		num_desc = []
+		q_dict = {}
 		for line in f:
 			line = line.rstrip()
 			if desc_bool is True:
 				desc = line
-				num_desc.append((num, desc))
+				q_dict[num] = desc
 				desc_bool = False
 			if "<num>" in line:
 				split_s = line.split()
 				num = int(split_s[len(split_s)-1])
 			if "<desc>" in line:
 				desc_bool = True
-		return num_desc
+		return q_dict
 
-# get question type for the current question
-# WHO / WHERE / WHEN
-#def get_question_type():
+def process_question(num, desc):
+	print num
+	print desc
 
 
-'''def get_inverted(desc):
-	# who / where / when
-	#  0  /   1   /  2
-	l = desc.split()
-	q_type = -1
-	inv_s = ''
-	print l
-	if l[0] == 'Who':
-		q_type = 0
-	if l[0] == 'Where':
-		q_type = 1
-	if l[0] is'When':
-		q_type = 2
-	print q_type
 
-	# original : Who is Colin Powell?
-	# modified : Colin Powell is
-	if q_type == 0:
-		for i in range(1, len(l)):
-			inv_s += l[i] + ' '
-		inv_s += l[1]
+def process_questions(q_dict):
+	for i in range(89, 90):
+		desc = q_dict[i]
+		process_question(i, desc)
 
-	if q_type == 1:
-		# has located? at the end
-		# original: Where is .. located?
-		# modified: .. is located in
-		if l[len(l)-1] == 'located?':
-			for i in range(2, len(l)-1):
-				inv_s += l[i] + ' '
-			inv_s += l[1]
-			inv_s += ' located in'
-		# does not have located? at the end
-		# original: Where is ..?
-		# modified: 
-		else:
-			for i in range(2, len(l)):
-				if i == len(l)-1:
-					l[i] = l[i].replace('?', '')
-				inv_s += l[i] + ' '
-			inv_s += l[1]
-			inv_s += ' located in'
-	#TODO
-	#if q_type == 2:
-	return inv_s
-'''
 
-def generate_answer(num_desc):
+
+'''def generate_answer(num_desc):
 	q_num, desc = num_desc
 	path = d_path + '/' + str(q_num)
-	#inv = get_inverted(desc)
-	#print inv
 	for name in glob.glob('path/*'):
 		with open(name) as f:
 			for line in f:
 				line = line.rstrip()
 	#print desc
+'''
 
 
+q_dict = get_questions()
+process_questions(q_dict)
 
-
-num_desc = get_questions()
-
-generate_answer(num_desc[0])
